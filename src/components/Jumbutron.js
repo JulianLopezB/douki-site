aa624ebb9b328cfdf0e7c80ccb3e5085e9e1935a
JulianLopezB <julianlopezb@gmail.com>
2022-11-18 00:46:43 -0300

    Adding text

--- src/components/Jumbutron.js
+++ src/components/Jumbutron.js
@@ -5,8 +5,9 @@ const Jumbutron = ({ children }) => {
     <div className="bg-amber-50 flex items-center py-10">
       <div className='max-w-md mx-auto w-full'>
         <h1 className='text-gray text-center text-3xl font-bold mb-5'>Fashionist.ai</h1>
-        <p className='text-gray text-left text-1xl  mb-3'>AI-driven experience to creatively shop for clothes by searching with natural language. 
+        <p className='text-gray text-left text-1xl  mb-1'>AI-driven experience to creatively shop for clothes by searching with natural language. 
         Images are sourced from Mango, Zara and H&M lastest collections. </p>
+        <p className='text-gray text-left text-1xl  mb-3'>Devloped by <a class="footer" href="https://twitter.com/JulianLBaasch" target="_blank">Julian Lopez Baasch</a></p>
         {children}
       </div>
     </div>
