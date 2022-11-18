d2c66e76a600b0bbf749a54dccd7b498e31a6bd4
JulianLopezB <julianlopezb@gmail.com>
2022-11-18 09:50:26 -0300

    chaning content

--- src/components/Jumbutron.js
+++ src/components/Jumbutron.js
@@ -5,7 +5,10 @@ const Jumbutron = ({ children }) => {
     <div className="bg-amber-50 flex items-center py-10">
       <div className='max-w-md mx-auto w-full'>
         <h1 className='text-gray text-center text-3xl font-bold mb-5'>Fashionist.ai</h1>
-        <p className='text-gray text-left text-1xl  mb-1'>AI-driven experience to creatively shop for clothes by searching with natural language. 
+        <p className='text-gray text-left text-1xl  mb-1'>
+          At Fashionist.Ai, we believe that fashion is an art. It's an audacious, ambitious and extremely personal project. </p>
+        <p className='text-gray text-left text-1xl  mb-1'>Our goal is to allow anyone to be stylish and fashionable by creating an easy shopping experience for them.</p>
+        <p className='text-gray text-left text-1xl  mb-3'>
         Images are sourced from Mango, Zara and H&M lastest collections. </p>
         <p className='text-gray text-left text-1xl  mb-3'>Devloped by <a class="footer" href="https://twitter.com/JulianLBaasch" target="_blank">Julian Lopez Baasch</a></p>
         {children}
