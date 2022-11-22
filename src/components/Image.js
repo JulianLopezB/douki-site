6903480cac9d4a535a8e6af010e7ed44f57012eb
JulianLopezB <julianlopezb@gmail.com>
2022-11-22 00:14:09 -0300

    new display of pins and adding price data to items

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,11 @@
-
 const Image = ({ data }) => {
   return (
     // <a href={data.img_urls} target="_blank" rel="noreferrer">
     //   <img className="h-72 w-full object-cover rounded-lg shadow-md" src={data.img_urls} alt={data.name} />
     // </a>
     
-      <div class="relative mb-4 before:content-[''] before:rounded-md before:absolute before:inset-0 before:bg-black before:bg-opacity-10">
+
+      <div class="relative  mb-4 before:content-[''] before:rounded-md before:absolute before:inset-0 before:bg-black before:bg-opacity-10">
         <img class="w-full rounded-md rounded-lg shadow-md object-cover" 
               src={data.img_urls}
               alt={data.name}
@@ -14,7 +14,8 @@ const Image = ({ data }) => {
           <div class="relative">
             {/* <a class="test__link absolute inset-0" target="_blank" href="/"></a> */}
             <h1 class="test__title text-1xl font-bold mb-1">{data.name}</h1>
-            <p class="test__author font-sm font-light">{data.brand}</p>
+            <p class="test__author font-sm font-light">{data.brand.toUpperCase()}</p>
+            <p class="test__author font-sm font-light">29 €</p>
           </div>
           <div class="mt-auto">
             <span class="test__tag bg-white bg-opacity-60 py-1 px-4 rounded-md text-black">See in shop</span>
@@ -25,5 +26,4 @@ const Image = ({ data }) => {
 }
 
 export default Image
-
 
--- src/components/Images.js
+++ src/components/Images.js
@@ -8,6 +8,7 @@ const Images = () => {
 
   return (
     <>
+      {/* <div class="container container-masonry"> */}
       {/* <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 max-w-7xl mx-auto px-4"> */}
       <div class="columns-2 md:columns-3 lg:columns-4">
         {isLoading ? <Skeleton item={10} /> : response.map((data, key) => <Image key={key} data={data} />)}
