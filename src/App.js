f771b6f05c2ee1b3d4aca7650909672c8cd652bc
JulianLopezB <julianlopezb@gmail.com>
2022-11-24 02:55:07 -0300

    changing placeholder

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export const ImageContext = createContext();
 
 function App() {
   const [searchImage, setSearchImage] = useState('');
-  const { response, isLoading, error, fetchData } = useAxios(`search?query=girl walking on the streets of Paris`);
+  const { response, isLoading, error, fetchData } = useAxios(`search?query=Wear like a character of a movie by Wes Anderson`);
 
   const value = {
     response,
