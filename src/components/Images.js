36879cc473ff5d865d007fef05f0749aae6f0a1b
JulianLopezB <julianlopezb@gmail.com>
2022-11-24 09:42:54 -0300

    Fix in md display

--- src/components/Images.js
+++ src/components/Images.js
@@ -8,11 +8,13 @@ const Images = () => {
   const { response, isLoading } = useContext(ImageContext);
 
   return (
-    <div className="flex items-center px-20">
-      {/* <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 max-w-7xl mx-auto px-4"> */}
-      <div class="columns-2 md:columns-3 lg:columns-4">
-        {/* {isLoading ? <Skeleton item={30} /> : response.map((data, key) => <Image key={key} data={data} />)} */}
-        {isLoading ? <Spin/> : response.map((data, key) => <Image key={key} data={data} />)}
+    <div className="items-center px-8">
+        <div className="md:shrink-0 items-center">
+        {/* <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 max-w-7xl mx-auto px-4"> */}
+        <div class="columns-2 md:columns-3 lg:columns-4">
+          {/* {isLoading ? <Skeleton item={30} /> : response.map((data, key) => <Image key={key} data={data} />)} */}
+          {isLoading ? <Spin/> : response.map((data, key) => <Image key={key} data={data} />)}
+        </div>
       </div>
     </div>
   )
