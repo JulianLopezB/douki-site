0fa8ed07543a848bdf606115d17987e90979e5f7
JulianLopezB <julianlopezb@gmail.com>
2022-11-29 01:17:16 -0300

    removing duplicated images from api response array

--- src/components/Images.js
+++ src/components/Images.js
@@ -75,6 +75,13 @@ function getImageSrc(brand) {
 function Images() {
 
   const { response  } = useContext(ImageContext);
+  const uniqueArray = response.filter((value, index) => {
+    const _value = JSON.stringify(value);
+    return index === response.findIndex(response => {
+      return JSON.stringify(response) === _value;
+    });
+  });
+  console.log(uniqueArray)
   // const [expanded, setExpanded] = React.useState(false);
   // const classes = useStyles();
   // const handleExpandClick = () => {
@@ -84,10 +91,10 @@ function Images() {
   return (
     
   
-    <div className="items-center px-80">
+    <div className="items-center px-8">
       <div className="md:shrink-0 items-center">
-        <div class="columns-2 md:columns-3 lg:columns-4">
-          {response.map((data, key) => (
+        <div class="columns-1 sm:columns-2 md:columns-3 lg:columns-4">
+          {uniqueArray.map((data, key) => (
             // <div class="relative  mb-4 before:content-[''] before:rounded-md before:absolute before:inset-0 before:bg-black before:bg-opacity-10">
             //   <img class="w-full rounded-md rounded-lg shadow-md object-cover" 
             //     src={data.img_urls}
