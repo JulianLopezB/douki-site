f44a847caf9ea404867a23e3827d8b632ebad68e
JulianLopezB <julianlopezb@gmail.com>
2022-11-29 22:08:20 -0300

    adding rel to href image button

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,28 +1,12 @@
 import { useContext } from "react"
 import { ImageContext } from "../App"
 import * as React from 'react';
-// import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
-// import CardContent from '@mui/material/CardContent';
-// import CardActions from '@mui/material/CardActions';
 import CardActionArea from '@material-ui/core/CardActionArea';
-// import Collapse from '@mui/material/Collapse';
-// import Avatar from '@mui/material/Avatar';
 import IconButton  from '@mui/material/IconButton';
-// import Typography from '@mui/material/Typography';
-// import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-// import ButtonBase from '@material-ui/core/ButtonBase';
-// import ShareIcon from '@mui/icons-material/Share';
-// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-// import MoreVertIcon from '@mui/icons-material/MoreVert';
-// import { makeStyles } from '@material-ui/core/styles';
-// import StorefrontIcon from '@mui/icons-material/Storefront';
-// interface ExpandMoreProps extends IconButtonProps {
-//   expand: boolean;
-// }
 
 
 function toTitleCase(str) {
@@ -46,34 +30,6 @@ function getImageSrc(brand) {
   return src
 }
 
-// const ExpandMore = styled((props: ExpandMoreProps) => {
-//   const { expand, ...other } = props;
-//   return <IconButton {...other} />;
-// })(({ theme, expand }) => ({
-//   transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-//   marginLeft: 'auto',
-//   transition: theme.transitions.create('transform', {
-//     duration: theme.transitions.duration.shortest,
-//   }),
-// }));
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: 'flex',
-//     '& > *': {
-//       margin: theme.spacing(1),
-//     },
-//   },
-//   small: {
-//     width: theme.spacing(3),
-//     height: theme.spacing(3),
-//   },
-//   large: {
-//     width: theme.spacing(7),
-//     height: theme.spacing(7),
-//   },
-// }));
-
 
 function Images() {
 
@@ -84,12 +40,6 @@ function Images() {
       return JSON.stringify(response) === _value;
     });
   });
-  console.log(uniqueArray)
-  // const [expanded, setExpanded] = React.useState(false);
-  // const classes = useStyles();
-  // const handleExpandClick = () => {
-  //   setExpanded(!expanded);
-  // };
 
   return (
     
@@ -122,7 +72,7 @@ function Images() {
             />
 
               <CardActionArea 
-              href={data.shop_link} target="_blank">
+              href={data.shop_link} target="_blank" rel="noopener noreferrer">
                 <CardMedia
                   component="img"
                   height="194"
