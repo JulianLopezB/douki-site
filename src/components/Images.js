05e0be4ef7f28ed9634f6b9c83ae72e964ba75a8
JulianLopezB <julianlopezb@gmail.com>
2022-12-01 18:49:56 -0300

    adding new brand source

--- src/components/Images.js
+++ src/components/Images.js
@@ -24,6 +24,8 @@ function getImageSrc(brand) {
     src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Zara_Logo.svg/320px-Zara_Logo.svg.png";
   } else if (brand === 'MANGO') {
     src = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Logo_of_Mango_%28new%29.svg/320px-Logo_of_Mango_%28new%29.svg.png";
+  } else if (brand === 'UO') {
+    src = "https://seeklogo.com/images/U/urban-outfitters-logo-E9B99ABE57-seeklogo.com.png";
   } else {
     src = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/320px-H%26M-Logo.svg.png";
   }
@@ -48,7 +50,6 @@ function Images() {
       <div className="md:shrink-0 items-center">
         <div class="columns-1 sm:columns-2 md:columns-3 lg:columns-4">
           {uniqueArray.map((data, key) => (
-
             <div class="relative  mb-4 before:content-[''] before:rounded-md before:absolute before:inset-0 before:bg-black before:bg-opacity-10">
             <Card 
             >
