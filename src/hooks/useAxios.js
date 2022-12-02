a44583a8053e778b7b88d465355912a8c19b9080
JulianLopezB <julianlopezb@gmail.com>
2022-12-01 23:06:25 -0300

    new index

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -6,7 +6,7 @@ const useAxios = (param) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
   
-  var emptyArray = new Array();
+  // var emptyArray = new Array();
 
   // axios.defaults.baseURL = 'https://clipfashion-gr7fp45wya-ue.a.run.app/api/v1/';
   axios.defaults.baseURL = 'http://127.0.0.1:5000/api/v1/';
