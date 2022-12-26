c0c826d512a69d628af56d6838359c3af8641423
JulianLopezB <julianlopezb@gmail.com>
2022-12-25 22:10:35 -0300

    new navbar style

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,5 +1,6 @@
 import { useContext } from "react"
-import { ImageContext } from "../App";
+import { ImageContext } from "../App"
+import {} from '../styles.css'
 
 const SearchField = () => {
   const { fetchData, setSearchImage, searchValue, setSearchValue, placeHolder, setPlaceHolder } = useContext(ImageContext);
@@ -7,12 +8,12 @@ const SearchField = () => {
   const handleInputChange = (e) => {
     setSearchValue(e.target.value);
   }
-  const handleButtonSearch = () => {
-    fetchData(`search?query=${searchValue}`)
-    setPlaceHolder(searchValue);
-    setSearchValue("");
-    setSearchImage(searchValue);
-  }
+  // const handleButtonSearch = () => {
+  //   fetchData(`search?query=${searchValue}`)
+  //   setPlaceHolder(searchValue);
+  //   setSearchValue("");
+  //   setSearchImage(searchValue);
+  // }
   const handleEnterSearch = e => {
     if(e.key === 'Enter') {
       fetchData(`search?query=${searchValue}`)
@@ -23,23 +24,39 @@ const SearchField = () => {
   }
 
   return (
-    <div className="flex">
-      <input
-        className="bg-gray-50 border border-gray-300 text-sm w-full indent-2 p-2.5 outline-none focus:border-blue-500 focus:ring-2 rounded-tl rounded-bl"
+    // <div className="flex">
+    //   <input
+    //     className="bg-gray-50 placeholder:italic border border-gray-300 text-sm w-full indent-2 p-2.5 outline-none focus:border-blue-500 focus:ring-2 rounded-tl rounded-bl"
+    //     type="search"
+    //     placeholder={placeHolder || "eg. Wes Anderson"}
+    //     value={searchValue}
+    //     onChange={handleInputChange}
+    //     onKeyDown={handleEnterSearch}
+    //     />
+    //   <button
+    //     onClick={handleButtonSearch}
+    //     disabled={!searchValue}
+    //     className="bg-blue-600 px-6 py-2.5 text-white rounded-tr rounded-br focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400"
+    //   >
+    //     Search
+    //   </button>
+    //   <div className="flex">
+    //   </div>
+      
+    // </div>
+
+    <div className="flexbox">
+      <div class="search">
+        <div>
+        <input
+        className="bg-gray-50 placeholder:italic border border-gray-300 text-sm w-full indent-2 p-2.5 outline-none focus:border-blue-500 focus:ring-2 rounded-tl rounded-bl"
         type="search"
         placeholder={placeHolder || "eg. Wes Anderson"}
         value={searchValue}
         onChange={handleInputChange}
         onKeyDown={handleEnterSearch}
         />
-      <button
-        onClick={handleButtonSearch}
-        disabled={!searchValue}
-        className="bg-blue-600 px-6 py-2.5 text-white rounded-tr rounded-br focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400"
-      >
-        Search
-      </button>
-      <div className="flex">
+        </div>
       </div>
       
     </div>
